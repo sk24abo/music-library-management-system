634dd54b6b4d9b6b5afb116643e186a634ce2fc2
sk24abo <sk24abo@herts.ac.uk>
2026-02-25 16:40:00 +0000

    Removed IDE template header and cleaned class documentation

--- music-library-management-system/Song.java
+++ music-library-management-system/Song.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 /**
  *This class represents a simple song in the music application.
@@ -55,3 +52,4 @@ public String getGenre() {
         return title + "-" + artist + "(" + runTimeSeconds + "s. " + genre + ")";   
     }
 }
+
