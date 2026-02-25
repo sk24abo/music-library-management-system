658ff91f8282220de943b56edeeb6c058dbbaabe
sk24abo <sk24abo@herts.ac.uk>
2026-02-25 16:39:27 +0000

    Removed IDE template header and cleaned class documentation

--- music-library-management-system/Playlist.java
+++ music-library-management-system/Playlist.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 // Importing ArrayList
 import java.util.ArrayList;
@@ -118,3 +115,4 @@ public ArrayList<Song> searchByGenre(String genre) {
 }
 
 */
+
